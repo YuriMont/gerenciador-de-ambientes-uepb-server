73dd9d1148c61b01746e2bccd943618b04415648
João Vitor <140824506+joaovlsousa@users.noreply.github.com>
2026-01-29 20:14:36 -0300

    Save environments use case (#3)
    * adicionar caso de uso (cadastrar novo ambiente)
    
    * adicionar documentação para caso de uso (cadastrar novo ambiente)

---
+++ src/main/java/dev/uepb/gereciador/ambientes/controller/EnvironmentController.java
@@ -0,0 +1,35 @@
+package dev.uepb.gereciador.ambientes.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import dev.uepb.gereciador.ambientes.dto.resquest.CreateEnvironmentRequest;
+import dev.uepb.gereciador.ambientes.entity.Environment;
+import dev.uepb.gereciador.ambientes.service.EnvironmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/environments")
+@Tag(name = "Environments")
+public class EnvironmentController {
+
+    @Autowired
+    private EnvironmentService environmentService;
+
+    @Operation(summary = "Create new environment")
+    @PostMapping
+    public ResponseEntity<Environment> create(
+        @Valid @RequestBody CreateEnvironmentRequest createEnvironmentRequest
+    ) {
+        Environment environment = environmentService.create(createEnvironmentRequest);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(environment);
+    }
+}
