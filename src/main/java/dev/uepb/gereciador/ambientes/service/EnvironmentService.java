0b5198154ed4228f61d266391a70ffd275fea436
João Vitor <140824506+joaovlsousa@users.noreply.github.com>
2026-01-30 18:35:09 -0300

    feat: add update environments use case (#8)

--- src/main/java/dev/uepb/gereciador/ambientes/service/EnvironmentService.java
+++ src/main/java/dev/uepb/gereciador/ambientes/service/EnvironmentService.java
@@ -1,11 +1,11 @@
 package dev.uepb.gereciador.ambientes.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import dev.uepb.gereciador.ambientes.dto.resquest.CreateEnvironmentRequest;
+import org.springframework.web.server.ResponseStatusException;
+import dev.uepb.gereciador.ambientes.dto.resquest.SaveEnvironmentRequest;
 import dev.uepb.gereciador.ambientes.entity.Environment;
 import dev.uepb.gereciador.ambientes.repository.EnvironmentRepository;
 
@@ -15,8 +15,20 @@ public class EnvironmentService {
     @Autowired
     private EnvironmentRepository environmentRepository;
 
-    public Environment create(CreateEnvironmentRequest createEnvironmentRequest) {
+    public Environment create(SaveEnvironmentRequest createEnvironmentRequest) {
         Environment environment = new Environment();
+
+        environment.setName(createEnvironmentRequest.name());
+        environment.setDescription(createEnvironmentRequest.description());
+
+        environmentRepository.save(environment);
+
+        return environment;
+    }
+
+    public Environment update(Long environmentId, SaveEnvironmentRequest createEnvironmentRequest) {
+        Environment environment = environmentRepository.findById(environmentId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         environment.setName(createEnvironmentRequest.name());
         environment.setDescription(createEnvironmentRequest.description());
